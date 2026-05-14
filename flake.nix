27fb76d29048917e692f8d3abe7cd9f4e31e0fb8
Sivak <141945552+sivakwork@users.noreply.github.com>
2026-05-13 23:58:55 -0400

    update sum stuff

--- flake.nix
+++ flake.nix
@@ -15,14 +15,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs/24.05";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/master";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, spicetify-nix, nixpkgs-24_05, minegrub-theme, sops-nix }:
+  outputs = inputs@{ self, nixpkgs, home-manager, spicetify-nix, nixpkgs-24_05 }:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -30,12 +27,10 @@
       
       modules = [
         ./hosts/nixos/default.nix
-        minegrub-theme.nixosModules.default
         home-manager.nixosModules.home-manager 
         {
           home-manager.extraSpecialArgs = { inherit inputs; };
         }
-        sops-nix.nixosModules.sops
       ];
     };
 
