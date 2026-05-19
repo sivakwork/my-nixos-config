adbca85c5bd6725c37ee3a536394a3a3dfdb1f71
Sivak <141945552+sivakwork@users.noreply.github.com>
2026-05-19 00:31:06 -0400

    hotfix

--- hosts/server/modules/host-config/hardware-configuration/extend.nix
+++ hosts/server/modules/host-config/hardware-configuration/extend.nix
@@ -2,12 +2,11 @@
 
 {
   networking.hostId = "52c538a2";
-  boot.initrd = availableKernelModules = [ "ehci_pci" "ata_piix" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ata_piix" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs = {
-    forceImportRoot = false;
+    forceImportRoot = true;
     devNodes = "/dev/disk/by-id";
-    extraPools = [ "rpool" ];
   };
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
