06d280bf9ac7dabc2d45cb92dbd1696c4939b16b
Sivak <141945552+sivakwork@users.noreply.github.com>
2026-05-20 01:04:01 -0400

    fix power thing

--- hosts/laptop/modules/laptop-optimizations.nix
+++ hosts/laptop/modules/laptop-optimizations.nix
@@ -1,16 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
+  services.power-profiles-daemon.enable = true;
   services.thermald.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-  };
 }
