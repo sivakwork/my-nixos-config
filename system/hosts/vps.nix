3ed4d546f2829f52f4a2944b59c00f7a1d92c4b1
Sivak <me@sivak.work>
2026-06-07 03:47:52 -0400

    fix oppsies

--- system/hosts/vps.nix
+++ system/hosts/vps.nix
@@ -3,7 +3,7 @@
 let
   mkHost = import ../../lib/mk_host.nix { inherit lib; inherit inputs; inherit config; };
 in {
-  flake.nixosConfigurations.nixvm = mkHost "vps" {
+  flake.nixosConfigurations.vps = mkHost "vps" {
     specialArgs = {
       isDesktop = false;
     }; 
