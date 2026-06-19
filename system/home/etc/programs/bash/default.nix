78ea2ba704741a5834dc601224bab6bb77cc2d81
Sivak <me@sivak.work>
2026-06-19 10:12:17 -0400

    alias changes

--- system/home/etc/programs/bash/default.nix
+++ system/home/etc/programs/bash/default.nix
@@ -10,13 +10,25 @@
         };
 
         initExtra = ''
-            rebuild-remote() {
+            rebuild-local() {
                 nixos-rebuild switch --flake .#''$1 --target-host root@''$1.local
             }
             nixcmd-gc-remote() {
                 ssh ''$1.local nix-collect-garbage -d
             }
             export PS1='\[\e[38;5;33;1m\]\u\[\e[0m\] \[\e[3m\]in\[\e[0m\] \[\e[38;5;202m\]\w\[\e[0m\] \$ '
+            rebuild-remote() {
+                case "''$1" in
+                    server) ip=10.100.0.1 ;;
+                    vps)    ip=10.100.0.2 ;;
+                    node)   ip=10.100.0.3 ;;
+                    nixos)  ip=10.100.0.4 ;;
+                    laptop) ip=10.100.0.5 ;;
+                    *) echo "Unknown host: $1"; return 1 ;;
+                esac
+
+                nixos-rebuild switch --flake .#''$1 --target-host root@''$ip
+            }
         '';
     };
 }
