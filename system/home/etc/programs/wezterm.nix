3c95aaa3a41221fa55245a8d7080f89240f8a017
Sivak <me@sivak.work>
2026-07-07 01:05:33 -0400

    tweak some

--- system/home/etc/programs/wezterm.nix
+++ system/home/etc/programs/wezterm.nix
@@ -32,12 +32,6 @@ if hostName == "nixos" then {
         local overrides = window:get_config_overrides() or {}
 
         if value == "0" then
-          overrides.window_padding = {
-            left = 0,
-            right = 0,
-            top = 0,
-            bottom = 0,
-          }
           overrides.window_background_opacity = 1
           overrides.colors = overrides.colors or {}
           overrides.colors.background = "#0d1117"
